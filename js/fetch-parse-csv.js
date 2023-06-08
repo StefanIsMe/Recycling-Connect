da11504e2bed6cd5ade195f58fc7efb88f34998d
Stefan Carter <130151819+StefanIsMe@users.noreply.github.com>
2023-06-08 16:41:59 +0700

    Update fetch-parse-csv.js

--- js/fetch-parse-csv.js
+++ js/fetch-parse-csv.js
@@ -1,3 +1,6 @@
+// Declare the map variable (accessible globally)
+var map;
+
 // Fetch and parse the CSV file
 Papa.parse("../db/collection.csv", {
   download: true,
@@ -7,6 +10,16 @@ Papa.parse("../db/collection.csv", {
 
     // Process the parsed CSV data and add markers to the map
     var data = results.data;
+    addMarkersToMap(data);
+  },
+  error: function(error) {
+    console.log("Error parsing CSV file:", error);
+  }
+});
+
+function addMarkersToMap(data) {
+  // Check if the map variable is defined and valid
+  if (map) {
     data.forEach(function(item) {
       var name = item.name;
       var lat = parseFloat(item.latitude);
@@ -16,8 +29,7 @@ Papa.parse("../db/collection.csv", {
       var marker = L.marker([lat, lng]).addTo(map);
       marker.bindPopup("<b>" + name + "</b><br>" + description);
     });
-  },
-  error: function(error) {
-    console.log("Error parsing CSV file:", error);
+  } else {
+    console.log("Map is not initialized.");
   }
-});
+}
