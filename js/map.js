8305771a271fcc5a38a4a7691a5792c810e20618
Stefan Carter <130151819+StefanIsMe@users.noreply.github.com>
2023-06-08 15:55:29 +0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -30,31 +30,26 @@ function initializeMap() {
     }
   });
 
-  // Check if the Geolocation API is supported
-  if ("geolocation" in navigator) {
-    // Function to handle geolocation success
-    function success(position) {
-      console.log("Geolocation successful:", position);
+  // Function to handle geolocation success
+  function success(position) {
+    console.log("Geolocation successful:", position);
 
-      var lat = position.coords.latitude;
-      var lng = position.coords.longitude;
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
 
-      map.setView([lat, lng], 13);
+    map.setView([lat, lng], 13);
 
-      // Create a marker at the user's location
-      L.marker([lat, lng]).addTo(map);
-    }
-
-    // Function to handle geolocation error
-    function error(error) {
-      console.log("Error accessing geolocation:", error);
-    }
-
-    // Call the geolocation API to get the user's location
-    navigator.geolocation.getCurrentPosition(success, error);
-  } else {
-    console.log("Geolocation API is not supported.");
+    // Create a marker at the user's location
+    L.marker([lat, lng]).addTo(map);
   }
+
+  // Function to handle geolocation error
+  function error(error) {
+    console.log("Error accessing geolocation:", error);
+  }
+
+  // Call the geolocation API to get the user's location
+  navigator.geolocation.getCurrentPosition(success, error);
 }
 
 // Call the initializeMap function when the page has finished loading
