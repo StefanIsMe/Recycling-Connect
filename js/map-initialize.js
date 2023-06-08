39d4d3790ba37d5cb5c50114aeb02f2c8d07f995
Stefan Carter <130151819+StefanIsMe@users.noreply.github.com>
2023-06-08 16:49:05 +0700

    Update map-initialize.js

--- js/map-initialize.js
+++ js/map-initialize.js
@@ -1,6 +1,7 @@
 // Wrap the code in a function for better encapsulation
 function initializeMap() {
-  var map = L.map("map").setView([51.505, -0.09], 13);
+  // Make the map variable global
+  map = L.map("map").setView([51.505, -0.09], 13);
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
