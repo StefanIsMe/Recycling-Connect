90aa03cf68055652898336dc86e63d5ea3634ab2
Stefan Carter <130151819+StefanIsMe@users.noreply.github.com>
2023-06-08 17:25:53 +0700

    Update geolocation.js

--- js/geolocation.js
+++ js/geolocation.js
@@ -5,6 +5,8 @@ function success(position) {
   var lat = position.coords.latitude;
   var lng = position.coords.longitude;
 
+  console.log("Latitude: " + lat + ", Longitude: " + lng);
+
   map.setView([lat, lng], 13);
 }
 
@@ -13,5 +15,6 @@ function error(error) {
   console.log("Error accessing geolocation:", error);
 }
 
+// The following code can be removed from geolocation.js as it should be triggered by the button click event
 // Call the geolocation API to get the user's location
-navigator.geolocation.getCurrentPosition(success, error);
+// navigator.geolocation.getCurrentPosition(success, error);
