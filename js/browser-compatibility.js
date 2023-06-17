14cd77b70aa07d78398ef39a7b68e0068e4dd9a7
Stefan Carter <130151819+StefanIsMe@users.noreply.github.com>
2023-06-17 18:31:41 +0700

    Update browser-compatibility.js

--- js/browser-compatibility.js
+++ js/browser-compatibility.js
@@ -1,8 +1,11 @@
 // Detect if the browser is not Google Chrome
 var isNotChrome = window.navigator.userAgent.indexOf("Chrome") === -1;
 
-// Display a message for non-Chrome users
+// Display a message based on the browser
 if (isNotChrome) {
   var message = "This website works best in Google Chrome. Please switch to Chrome for the best experience.";
   alert(message);
+  console.log("The web browser is not Google Chrome.");
+} else {
+  console.log("The web browser is Google Chrome.");
 }
