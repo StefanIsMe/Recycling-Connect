c6c8a954ea15b1b0045aea4a573ed21a85eb62b5
Stefan Carter <130151819+StefanIsMe@users.noreply.github.com>
2023-06-17 18:57:26 +0700

    Update browser-compatibility.js

--- js/browser-compatibility.js
+++ js/browser-compatibility.js
@@ -1,20 +1,16 @@
 function browserCompatibilityCheck() {
   try {
-    // Check if the browser is not Microsoft Edge
-    var isNotEdge = !(navigator.userAgentData && navigator.userAgentData.brands && navigator.userAgentData.brands.some(function(brand) {
-      return brand.brand === 'Microsoft Edge';
-    }));
+    // Check if the browser is not Google Chrome
+    var isNotChrome = window.navigator.userAgent.indexOf("Chrome") === -1;
 
     // Display a message based on the browser
-    if (isNotEdge) {
+    if (isNotChrome) {
       var message = "This website works best in Google Chrome. Please switch to Chrome for the best experience.";
-      var error = new Error("An error occurred with the web browser compatibility JavaScript code detection");
-      error.details = message;
-      console.error(error);
+      console.error("The web browser is not Google Chrome.");
       hideContent();
       showMessage(message);
     } else {
-      console.log("The web browser is Microsoft Edge.");
+      console.log("The web browser is Google Chrome.");
     }
   } catch (error) {
     console.error("An unexpected error occurred with the web browser compatibility JavaScript code detection");
@@ -40,10 +36,7 @@ function hideContent() {
 function showMessage(message) {
   try {
     var bodyElement = document.getElementsByTagName("body")[0];
-    var h1Element = document.createElement("h1");
-    var textNode = document.createTextNode(message);
-    h1Element.appendChild(textNode);
-    bodyElement.appendChild(h1Element);
+    bodyElement.innerHTML = "<h1>" + message + "</h1>";
     console.log("Error message displayed:", message);
   } catch (error) {
     console.error("An unexpected error occurred while displaying the error message.");
