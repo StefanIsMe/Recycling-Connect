03201ee71ed133be4b647b5a1285e939e6e4e469
Stefan Carter <130151819+StefanIsMe@users.noreply.github.com>
2023-06-17 19:13:59 +0700

    Update browser-compatibility.js

--- js/browser-compatibility.js
+++ js/browser-compatibility.js
@@ -11,12 +11,34 @@ function browserCompatibilityCheck() {
       var error = new Error("An error occurred with the web browser compatibility JavaScript code detection");
       error.details = message;
       console.error(error);
+      hideContent();
       showMessage(message);
     } else {
       console.log("The web browser is Google Chrome.");
     }
   } catch (error) {
     console.error("An unexpected error occurred with the web browser compatibility JavaScript code detection");
+    console.error(error);
+  }
+}
+
+function hideContent() {
+  try {
+    var bodyElement = document.getElementsByTagName("body")[0];
+
+    if (!bodyElement) {
+      throw new Error("Body element not found");
+    }
+
+    // Hide all child elements of the body
+    while (bodyElement.firstChild) {
+      bodyElement.firstChild.style.display = "none";
+      bodyElement.removeChild(bodyElement.firstChild);
+    }
+
+    console.log("Content hidden.");
+  } catch (error) {
+    console.error("An unexpected error occurred while hiding content.");
     console.error(error);
   }
 }
