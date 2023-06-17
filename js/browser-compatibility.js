4626889d437ebb5185f1a0e65f1697602c1abfad
Stefan Carter <130151819+StefanIsMe@users.noreply.github.com>
2023-06-17 18:43:02 +0700

    Update browser-compatibility.js

--- js/browser-compatibility.js
+++ js/browser-compatibility.js
@@ -1,13 +1,22 @@
-// Detect if the browser is not Google Chrome
-var isNotChrome = !(navigator.userAgentData && navigator.userAgentData.brands && navigator.userAgentData.brands.some(function (brand) {
-  return brand.brand === 'Google Chrome';
-}));
+(function () {
+  try {
+    // Detect if the browser is not Google Chrome
+    var isNotChrome = !(window.chrome && window.chrome.webstore);
 
-// Display a message based on the browser
-if (isNotChrome) {
-  var message = "This website works best in Google Chrome. Please switch to Chrome for the best experience.";
-  alert(message);
-  console.log("The web browser is not Google Chrome.");
-} else {
-  console.log("The web browser is Google Chrome.");
-}
+    // Show/hide the content based on the browser
+    if (isNotChrome) {
+      var contentElement = document.getElementById('content');
+      contentElement.style.display = 'none';
+
+      var chromeMessage = document.createElement('h1');
+      chromeMessage.textContent = 'This website works best in Google Chrome. Please download Google Chrome for the best experience.';
+      document.body.appendChild(chromeMessage);
+
+      console.log('The web browser is not Google Chrome.');
+    } else {
+      console.log('The web browser is Google Chrome.');
+    }
+  } catch (error) {
+    console.error('An error occurred with the web browser compatibility JavaScript code detection:', error);
+  }
+})();
