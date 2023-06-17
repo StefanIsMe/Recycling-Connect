939b5e7cabc024cf9f984b1d5f5f4539c82524e7
Stefan Carter <130151819+StefanIsMe@users.noreply.github.com>
2023-06-17 18:45:02 +0700

    Update browser-compatibility.js

--- js/browser-compatibility.js
+++ js/browser-compatibility.js
@@ -1,22 +1,29 @@
-(function () {
-  try {
-    // Detect if the browser is not Google Chrome
-    var isNotChrome = !(window.chrome && window.chrome.webstore);
+function browserCompatibilityCheck() {
+  // Detect if the browser is not Google Chrome
+  var isNotChrome = window.navigator.userAgent.indexOf("Chrome") === -1;
 
-    // Show/hide the content based on the browser
-    if (isNotChrome) {
-      var contentElement = document.getElementById('content');
-      contentElement.style.display = 'none';
-
-      var chromeMessage = document.createElement('h1');
-      chromeMessage.textContent = 'This website works best in Google Chrome. Please download Google Chrome for the best experience.';
-      document.body.appendChild(chromeMessage);
-
-      console.log('The web browser is not Google Chrome.');
-    } else {
-      console.log('The web browser is Google Chrome.');
-    }
-  } catch (error) {
-    console.error('An error occurred with the web browser compatibility JavaScript code detection:', error);
+  // Display a message based on the browser
+  if (isNotChrome) {
+    var message = "This website works best in Google Chrome. Please switch to Chrome for the best experience.";
+    console.error("An error occurred with the web browser compatibility JavaScript code detection: " + message);
+    hideContent();
+    showMessage(message);
+  } else {
+    console.log("The web browser is Google Chrome.");
   }
-})();
+}
+
+function hideContent() {
+  var contentElement = document.getElementById("content");
+  if (contentElement) {
+    contentElement.style.display = "none";
+  }
+}
+
+function showMessage(message) {
+  var bodyElement = document.getElementsByTagName("body")[0];
+  var h1Element = document.createElement("h1");
+  var textNode = document.createTextNode(message);
+  h1Element.appendChild(textNode);
+  bodyElement.appendChild(h1Element);
+}
